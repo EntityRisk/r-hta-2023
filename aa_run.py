d6050457d3b4ec1f28525d76cf02e72344f80068
dincerti <devin.incerti@gmail.com>
2023-06-10 10:47:47 -0700

    refactor: use functions in Python script to facilitate unit testing

--- aa_run.py
+++ aa_run.py
@@ -2,23 +2,31 @@
 
 import functions as hta
 
-# Deterministic analysis
-trans_probs = hta.trans_prob_matrix()
-state_probs = hta.sim_markov(trans_probs, n_cycles=5)
-discounted_qalys = hta.compute_qalys(state_probs, qol=[0.8, 0.6, 0])
 
-print(trans_probs)
-print(state_probs)
-print(discounted_qalys)
+def run_deterministic():
+    """Run the deterministic model."""
+    trans_probs = hta.trans_prob_matrix()
+    state_probs = hta.sim_markov(trans_probs, n_cycles=5)
+    discounted_qalys = hta.compute_qalys(state_probs, qol=[0.8, 0.6, 0])
 
-# Probabilistic sensitivity analysis
-SAMPLE_SIZE = 100
-trans_data = (trans_probs * SAMPLE_SIZE).astype(int)
-trans_probs_psa = hta.simulate_trans_probs(trans_data, n_sims=3)
-state_probs_psa = hta.sim_markov_psa(trans_probs_psa, n_cycles=5)
-state_probs_psa = hta.label_state_probs(state_probs_psa)
+    print(trans_probs)
+    print(state_probs)
+    print(discounted_qalys)
 
-print(trans_data)
-print(trans_probs_psa)
-print(state_probs_psa)
-print(state_probs_psa.mean(dim="sim"))
+
+def run_psa():
+    """Run the probabilistic sensitivity analysis."""
+    sample_size = 100
+    trans_data = (hta.trans_prob_matrix() * sample_size).astype(int)
+    trans_probs_psa = hta.simulate_trans_probs(trans_data, n_sims=3)
+    state_probs_psa = hta.sim_markov_psa(trans_probs_psa, n_cycles=5)
+
+    print(state_probs_psa)
+    state_probs_psa = hta.label_state_probs(state_probs_psa)
+    print(state_probs_psa)
+    print(state_probs_psa.mean(dim="sim"))
+
+
+if __name__ == "__main__":
+    run_deterministic()
+    run_psa()
