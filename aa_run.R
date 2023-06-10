513043dea13f91fca0a4d33dd547f71c970901d9
dincerti <devin.incerti@gmail.com>
2023-06-09 17:45:26 -0700

    refactor: minor formatting edits for the slides

--- aa_run.R
+++ aa_run.R
@@ -1,10 +1,12 @@
 # Run analyses with R
 
-# R ----------------------------------------------------------------------------
 source("functions.R")
 trans_probs <- trans_prob_matrix()
 state_probs <- sim_markov(trans_probs, n_cycles = 5)
-discount_qalys <- compute_qalys(state_probs, qol = c(0.8, 0.6, 0))
+discounted_qalys <- compute_qalys(
+  state_probs,
+  qol = c(0.8, 0.6, 0)
+)
 
 print(trans_probs)
 print(state_probs)
